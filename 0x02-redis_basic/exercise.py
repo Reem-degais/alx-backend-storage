9088d2854b6973d530279dbf5b7917b2663fc44f
Reem-degais <reem.degais.33@gmail.com>
2024-03-09 00:02:07 +0000

    -

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,6 +3,8 @@
 Contains the class definition for redis cache
 """
 import redis
+from typing import Callable, Optional, Union
+from uuid import uuid4
 
 
 class Cache:
@@ -23,3 +25,31 @@ class Cache:
         randomKey = str(uuid4())
         self._redis.set(randomKey, data)
         return randomKey
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        '''
+            Get data from the cache.
+        '''
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        '''
+            Get a string from the cache.
+        '''
+        value = self._redis.get(key)
+        return value.decode('utf-8')
+
+    def get_int(self, key: str) -> int:
+        '''
+            Get an int from the cache.
+        '''
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode('utf-8'))
+        except Exception:
+            value = 0
+        return value
